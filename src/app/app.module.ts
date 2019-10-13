da86cb658df045dd6d9f6e61adb597b6010a300b
Filip Culig <filip.culig@gmail.com>
2019-10-13 15:55:23 +0200

    Started designing navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
+    HomePageComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-navigation-bar',
-  templateUrl: './navigation-bar.component.html',
-  styleUrls: ['./navigation-bar.component.scss']
+  selector: "app-navigation-bar",
+  templateUrl: "./navigation-bar.component.html",
+  styleUrls: ["./navigation-bar.component.scss"]
 })
 export class NavigationBarComponent implements OnInit {
+  scrolled: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  @HostListener("window:scroll", ["$event"])
+  onWindowScroll(e) {
+    let element = document.querySelector(".menu-bar");
+
+    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (number > 80) {
+      element.classList.add("scroll");
+      this.scrolled = true;
+    }else {
+      element.classList.remove("scroll");
+      this.scrolled = false
+    }
   }
-
 }
